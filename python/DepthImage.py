c076cdd21f3492167e6b3ad012be4158f12f6680
Guo.Xing <reveriel@hotmail.com>
2020-06-24 11:44:32 +0800

    update datastructure

--- python/DepthImage.py
+++ python/DepthImage.py
@@ -3,38 +3,32 @@ import random
 import numpy as np
 
 
-def generate_test_image(B, D, H, W, C, T):
+def generate_test_image(B, C, D, H, W, T):
     """
         B: batchsize
-        T: thickness, the max number of non-empty voxels on Depth
-        H, W, D: height,width, depth
         C, channel
+        D, H, W : depth, height, width
+        T: thickness, the max number of non-empty voxels on Depth
     """
     assert T <= D, "T must be less then D, got T={}, D={}".format(T, D)
-    feature = torch.randn(B, T, H, W, C)
-    indice_map = np.ndarray((B, H, W), dtype=dict)
-    # map from i to spatial indice
+    feature = torch.randn(B, C, T, H, W)
+    depth = torch.randint(D, (B, T, H, W))
     thick = torch.randint(T, (B, H, W))
 
+    # sort depth
     for b in range(B):
-        for x in range(H):
-            for y in range(W):
-                max_t = thick[b, x, y]
-                indice_map[b, x, y] = dict()
-                for t in range(max_t):
-                    z = random.randint(0, D-1)
-                    while z in indice_map[b, x, y]:
-                        z = random.randint(0, D-1)
-                    indice_map[b, x, y][t] = z
-    return DepthImage(feature, indice_map, thick, D)
+        for i in range(H):
+            for j in range(W):
+                values, _ = torch.sort(depth[b, :, i, j])
+                depth[b, :, i, j] = values
+
+    return DepthImage(feature, depth, thick, D)
 
 
 class DepthImage():
     """
-    two parts
-
     feature:
-        Tensor of shape [B, T, H, W, C]
+        Tensor of shape [B, C, T, H, W]
         TODO: C's position ?
             T: thickness
             B: batchsize
@@ -42,24 +36,24 @@ class DepthImage():
             H,W, height and width, two spatial demension
             D: Depth, the depth dimesion is stored compactly
 
-    indice_map, a matrix of hashmap of shape [B, H, W]
-        stores indice_map for each spatial location
-        maps from indice on 'T' to spatial location on 'D'
+    depth:
+        int Tensor of shape [B, T, H, W]
 
     thick: tensor of shape [B, H, W]
-        the max number of non empty voxels on 'D'
+        the max number of non empty voxels on 'D' dimension
     """
 
-    def __init__(self, feature: torch.Tensor, indice_map, thick, D):
-        B, T, H, W, C = feature.shape
-        B_i, H_i, W_i = indice_map.shape
+    def __init__(self, feature: torch.Tensor, depth, thick, D):
+        B, _, T, H, W = feature.shape
+        B_d, T_d, H_d, W_d = depth.shape
         B_t, H_t, W_t = thick.shape
-        assert (B == B_i == B_t and H == H_i == H_t and W == W_i == W_t), \
-            "dimension not match, feature.shape={}, indice_map.shape={}, thick.shape={}"\
-            .format(feature.shape, (B_i, H_i, W_i), thick.shape)
+        assert (B == B_d == B_t and H == H_d == H_t and W == W_d == W_t and
+                T == T_d), \
+            "dimension not match, feature.shape={}, depth.shape={}, thick.shape={}"\
+            .format(feature.shape, (B_d, H_d, W_d), thick.shape)
 
         self.feature = feature
-        self.indice_map = indice_map
+        self.depth = depth
         self.thick = thick
         self.D = D
 
@@ -68,23 +62,22 @@ class DepthImage():
         return a 3D tensor of shape (batchsize, D, H, W, C)
             where D = max_dpeth if specified
         """
-        return to_dense(self.feature, self.indice_map, self.thick, self.D)
+        return to_dense(self.feature, self.depth, self.thick, self.D)
 
 
-def to_dense(feature, indice_map, thick, D, device=None):
+def to_dense(feature, depth, thick, D, device=None):
     """Convert to dense 3D tensor
-    return a 3D tensor of shape (batchsize, D, H, W, C)
+    return a 3D tensor of shape (B, C, D, H, W)
         where D = max_dpeth if specified
     """
-    B, T, H, W, C = feature.shape
-    buffer = torch.zeros((B, D, H, W, C), device=device)
+    B, C, T, H, W = feature.shape
+    buffer = torch.zeros((B, C, D, H, W), device=device)
     for b in range(B):
         for x in range(H):
             for y in range(W):
-                max_t = thick[b, x, y]
-                for t in range(max_t):
-                    z = indice_map[b, x, y][t]
-                    buffer[b, z, x, y] = feature[b, t, x, y]
+                for t in range(thick[b, x, y]):
+                    z = depth[b, t, x, y]
+                    buffer[b, :, z, x, y] = feature[b, :, t, x, y]
     return buffer
 
 
@@ -95,7 +88,7 @@ def test_dense():
 
 
 def check(input: DepthImage):
-    B, T, H, W, C = input.feature.shape
+    B, C, T, H, W = input.feature.shape
     D = input.D
 
     dense = input.dense()
@@ -107,10 +100,11 @@ def check(input: DepthImage):
     for b in range(B):
         for x in range(H):
             for y in range(W):
-                max_t = input.thick[b, x, y]
-                for t in range(max_t):
-                    z = input.indice_map[b, x, y][t]
-                    if sum(abs(dense[b, z, x, y] - input.feature[b, t, x, y])) > 1e-6:
+                for t in range(input.thick[b, x, y]):
+                    z = input.depth[b, t, x, y]
+                    if sum(abs(dense[b,:, z, x, y] - input.feature[b,:, t, x, y])) > 1e-6:
+                        print("dense[",b,":", z, x,y,"]=", dense[b,:,z,x,y])
+                        print("feature[",b,":", t, x,y,"]=", input.feature[b,:,t,x,y])
                         return False
     return True
 
