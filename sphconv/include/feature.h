efdc7ae7aec6e58c019c2293f15051236341e065
Guo.Xing <reveriel@hotmail.com>
2021-01-09 09:53:09 +0800

    tensor dimension, static to dynamic

--- sphconv/include/feature.h
+++ sphconv/include/feature.h
@@ -17,36 +17,24 @@
  *
  * @tparam T
  * @tparam Index
- * @tparam B_
- * @tparam H_
- * @tparam W_
- * @tparam D_
- * @tparam C_
  */
 template <typename T,
-          typename Index,
-          int B_,
-          int H_,
-          int W_,
-          int D_,
-          int C_
+          typename Index
           >
 struct Feature
 {
     // NNZ denote the number of non-zero elements
+    unsigned int const B;
+    unsigned int const H;
+    unsigned int const W;
+    unsigned int const D;
+    unsigned int const C;
 
-    static unsigned int const B = B_;
-    static unsigned int const H = H_;
-    static unsigned int const W = W_;
-    static unsigned int const D = D_;
-    static unsigned int const C = C_;
-
-    using ChannelVec = T[C];
     // all nonzeros are in the 'val'
     // size: NNZ * C
     // the 'val' stores all nonzeros
     // its a pointer points to an array of
-    ChannelVec *val;
+    T *val;
 
     // the 'z_ind' stores the z indexes of the elements in 'val'
     // if val[k] = A[b,x,y,z,c], then z_ind[k]  = z
@@ -55,8 +43,14 @@ struct Feature
 
     // the 'z_ptr' stores the locations in 'val' that start a 'C' vector.
     // if val[k] = A[b,x,y,z,c], then  z_ptr[b,x,y] <= k < z_ptr[b,x,y + 1]
-    // size: B * H * W
-    Index z_ptr[B][H][W];
+    // equivalent to
+    //     Index z_ptr[B][H][W];
+    // but B H W is not static constant, we manage it at runtime
+    // and we append a guard at z_ptr[B-1][H-1][W]
+    //
+    // size: B * H * W + 1
+    Index *z_ptr;
+
     // the 'nnz' also act as a guard, z_ptr[B-1][H-1][W] read this
     // WARN: this field must be next to z_ptr
     Index nnz;
@@ -73,22 +67,37 @@ private:
         if (capacity == 0) {
             // first time allocate
             capacity = MIN_capacity;
-            val = (T (*) [C]) malloc(sizeof(T) * capacity * C);
+            val = (T*)malloc(sizeof(T) * capacity * C);
             z_ind = (Index *)malloc(sizeof(Index) * capacity);
             return;
         }
         capacity *= 2;
-        val = (T (*) [C]) realloc((void*)val, sizeof(T) * capacity * C);
+        val = (T *)realloc((void *)val, sizeof(T) * capacity * C);
         z_ind = (Index *)realloc((void*)z_ind, sizeof(Index) * capacity);
         return;
     }
 
     bool full() { return capacity * C >= nnz; }
 
+    inline
+    Index &z_ptr_at(Index b, Index x, Index y) {
+        return *(z_ptr + (((b * H) + x) * W + y));
+    }
+
+    inline
+    Index &z_ptr_end() {
+        return z_ptr_at(B-1, H-1, W);
+    }
+
+
 public:
-    Feature() : nnz(0), capacity(0), val(nullptr), z_ind(nullptr) {
-        memset(z_ptr, 0, sizeof(z_ptr));
-        assert(&nnz == &z_ptr[B-1][H-1][W]);
+    Feature(Index B_, Index H_, Index W_, Index D_, Index C_)
+        : B(B_), H(H_), W(W_), D(D_), C(C_),
+          nnz(0), capacity(0), val(nullptr), z_ind(nullptr)
+    {
+        z_ptr = (Index *)malloc(sizeof(Index) * B * H * W + 1);
+        memset(z_ptr, 0, sizeof(Index) * B * H * W + 1);
+        // assert(&nnz == &z_ptr[B - 1][H - 1][W]);
     }
 
     ~Feature() {
@@ -96,39 +105,40 @@ public:
             free(val);
         if (z_ind)
             free(z_ind);
+        free(z_ptr);
     }
 
     /**
      * @brief  get element, assume b,x,y,z,c is a valid index
      */
     T get(Index b, Index x, Index y, Index z, Index c) const {
-        Index z_start = z_ptr[b][x][y];
-        Index z_end = z_ptr[b][x][y+1];
+        Index z_start = z_ptr_at(b, x, y);
+        Index z_end = z_ptr_at(b, x, y + 1);
         // find equal z
         Index i = z_start;
         for (i = z_start; i < z_end; i++)
             if (z_ind[i] == z)
                 break;
-        return val[i][c];
+        return val[i * C + c];
     }
 
     T &get(Index b, Index x, Index y, Index z, Index c) {
-        Index z_start = z_ptr[b][x][y];
-        Index z_end = z_ptr[b][x][y+1];
+        Index z_start = z_ptr_at(b, x, y);
+        Index z_end = z_ptr_at(b, x, y + 1);
         // find equal z
         Index i = z_start;
         for (i = z_start; i < z_end; i++)
             if (z_ind[i] == z)
                 break;
-        return val[i][c];
+        return val[i * C + c];
     }
 
 
     bool has_idx(Index b, Index x, Index y, Index z, Index c) {
         if (b >= B || x >= H || y >= W || z >= D || c >= C) return false;
         if (nnz == 0) return false;
-        Index z_start = z_ptr[b][x][y];
-        Index z_end = z_ptr[b][x][y+1];
+        Index z_start = z_ptr_at(b, x, y);
+        Index z_end = z_ptr_at(b, x, y+1);
         for (Index i = z_start; i < z_end; i++)
             if (z_ind[i] == z)
                 return true;
@@ -143,8 +153,8 @@ public:
         if (full()) {
             expand();
         }
-        Index z_start = z_ptr[b][x][y];
-        Index z_end = z_ptr[b][x][y+1];
+        Index z_start = z_ptr_at(b, x, y);
+        Index z_end = z_ptr_at(b, x, y+1);
         // TODO: binary search
         Index i = z_start;
         for(; i < z_end; i++)
@@ -155,10 +165,13 @@ public:
             z_ind[j] = z_ind[j-1];
         }
         z_ind[i] = z;
-        val[i][c] = value;
-        for (Index *p = &z_ptr[b][x][y] + 1; p != &nnz; p++)
+        val[i * C + c] = value;
+        for (Index *p = &z_ptr_at(b, x, y + 1),
+                   *end = &z_ptr_end();
+             p != end; p++)
             *p += 1;
         nnz += 1;
+        z_ptr_end() = nnz;
         return;
     }
 
@@ -166,13 +179,13 @@ public:
         for (int b = 0; b < B; b++) {
             for (int x = 0; x < H; x++) {
                 for (int y = 0; y < W; y++) {
-                    for (Index start = z_ptr[b][x][y],
-                               end = z_ptr[b][x][y+1];
+                    for (Index start = z_ptr_at(b, x, y),
+                               end = z_ptr_at(b, x, y + 1);
                          start < end; start++)
                     {
                         for (int c = 0; c < C; c++) {
                             printf("[%d,%d,%d,%d,%d] -> %.2f\n",
-                            b, x, y, z_ind[start], c, val[start][c]);
+                            b, x, y, z_ind[start], c, val[start * C + c]);
                         }
                     }
                 }
--- sphconv/test/feature.cpp
+++ sphconv/test/feature.cpp
@@ -4,29 +4,31 @@
 
 #include <cstdint>
 
-void test() {
+void test()
+{
     {
-    Feature<float, uint32_t, 2, 2, 2, 2, 2> a0;
-    Feature<float, uint32_t, 2, 2, 2, 2, 4> a1;
-    Feature<float, uint32_t, 2, 2, 2, 7, 8> a2;
-    Feature<float, uint32_t, 2, 3, 2, 2, 2> a3;
-    Feature<float, uint32_t, 2, 2, 7, 2, 2> a4;
-    Feature<float, uint32_t, 1, 1, 1, 13, 1> a5;
-    Feature<float, uint32_t, 1, 1, 13, 13, 1> a6;
-    Feature<float, uint32_t, 1, 11, 13, 13, 1> a7;
+        Feature<float, uint32_t> a0(2,2,2,2,2);
+        Feature<float, uint32_t> a1(2,1,2,3,1);
+        Feature<float, uint32_t> a2(3,5,1,7,8);
+        Feature<float, uint32_t> a3(9,1,5,3,2);
+        Feature<float, uint32_t> a4(2,2,7,2,2);
+        Feature<float, uint32_t> a5(1,1,1,13,1);
+        Feature<float, uint32_t> a6(1,1,13,13,1);
+        Feature<float, uint32_t> a7(1,11,13,13,1);
     }
-    Feature<float, uint32_t, 2, 2, 2, 2, 2> a;
 
-    a.set(0,0,0,1,1, 24.f) ;
-    printf("get %.2f\n", a.get(0,0,0,1,1) );
+    Feature<float, uint32_t> a(2,2,2,2,2);
+
+    a.set(0, 0, 0, 1, 1, 24.f);
+    a.set(0, 0, 0, 1, 0, 23.f);
+    a.set(0, 1, 0, 1, 0, 21.f);
+    printf("get %.2f\n", a.get(0, 0, 0, 1, 1));
     a.print();
-
 }
 
-int main() {
+int main()
+{
     test();
     printf("hello\n");
     return 0;
-
-
 }
