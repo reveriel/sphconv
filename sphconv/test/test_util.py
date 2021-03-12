845e9b9fda8a7f5d40cb2819c958814cae6a882b
Guo.Xing <reveriel@hotmail.com>
2021-03-12 16:26:33 +0800

    remove old ELL code, add pytest

---
+++ sphconv/test/test_util.py
@@ -0,0 +1,20 @@
+# from sphconv.utils import *
+
+# point to raw voxelFeature
+# raw voxelFeature processed with VFE
+# voxel feature convert to sphconv.Feature and spconv.SparseTensor
+
+# to dense to check eqaulity
+
+# run convolution on both.
+
+class TestClass:
+    def test_uppr(self):
+        assert 'foo'.upper() == 'FO'
+
+
+
+
+
+
+
--- sphconv/test/test_utils.py
+++
@@ -1,5 +0,0 @@
-from sphconv.utils import *
-
-
-
-
